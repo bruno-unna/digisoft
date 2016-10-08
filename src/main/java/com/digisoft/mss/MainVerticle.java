69077f6757b53342889c8cc7b61c59e32109a1e2
Bruno Unna <bruno.unna@gmail.com>
2016-10-08 14:34:35 +0100

    Added docker boilerplate and the main verticle skeleton.

---
+++ src/main/java/com/digisoft/mss/MainVerticle.java
@@ -0,0 +1,9 @@
+package com.digisoft.mss;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * A verticle that works as a micro-service for the management of subscriptions.
+ */
+public class MainVerticle extends AbstractVerticle {
+}
