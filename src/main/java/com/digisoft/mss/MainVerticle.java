425e478e0e7ab4a4c73a61526e3d4e8cd4db842a
Bruno Unna <bruno.unna@gmail.com>
2016-10-09 14:30:51 +0100

    Added the routes and skeleton handlers for the defined API.

--- src/main/java/com/digisoft/mss/MainVerticle.java
+++ src/main/java/com/digisoft/mss/MainVerticle.java
@@ -14,6 +14,10 @@ import static io.netty.handler.codec.http.HttpResponseStatus.OK;
  * A verticle that works as a micro-service for the management of subscriptions.
  */
 public class MainVerticle extends AbstractVerticle {
+    private static final String DEFAULT_RABBIT_HOST = "rabbit";
+    private static final int DEFAULT_RABBIT_PORT = 5672;
+    private static final int DEFAULT_HTTP_PORT = 8080;
+
     private Logger logger = LoggerFactory.getLogger(MainVerticle.class);
 
     @Override
@@ -22,26 +26,28 @@ public class MainVerticle extends AbstractVerticle {
         // environment or from vertx configuration file
         String rabbitHost = System.getenv("RABBIT_HOST");
         if (rabbitHost == null) {
-            rabbitHost = config().getString("rabbit.host", "rabbit");
+            rabbitHost = config().getString("rabbit.host", DEFAULT_RABBIT_HOST);
         }
         logger.info("Using rabbit host " + rabbitHost);
 
         Integer rabbitPort = Integer.getInteger("RABBIT_PORT");
         if (rabbitPort == null) {
-            rabbitPort = config().getInteger("rabbit.port", 5672);
+            rabbitPort = config().getInteger("rabbit.port", DEFAULT_RABBIT_PORT);
         }
         logger.info("Using rabbit port " + rabbitPort);
 
         Integer httpPort = Integer.getInteger("HTTP_PORT");
         if (httpPort == null) {
-            httpPort = config().getInteger("http.port", 8080);
+            httpPort = config().getInteger("http.port", DEFAULT_HTTP_PORT);
         }
         logger.info("Using http port " + httpPort);
 
         // create the routes that are recognised by the service
         // and send requests to appropriate handler/catalog
         Router router = Router.router(vertx);
-        router.get("/").handler(this::handleRequest);
+        router.get("/subscriptions/:id").handler(this::handleGetSubscription);
+        router.put("/subscriptions").handler(this::handlePutSubscription);
+        router.post("/messages").handler(this::handlePostMessage);
 
         // finally, create the http server, using the created router
         vertx
@@ -49,23 +55,41 @@ public class MainVerticle extends AbstractVerticle {
                 .requestHandler(router::accept)
                 .listen(httpPort, result -> {
                     if (result.succeeded()) {
-                        logger.info("Proxy server started");
+                        logger.info("Messaging server started");
                         startFuture.complete();
                     } else {
-                        logger.error("Couldn't start proxy server");
+                        logger.error("Couldn't start messaging server");
                         startFuture.fail(result.cause());
                     }
                 });
     }
 
     /**
-     * This method is HTTP-related, and is responsible for handling the
-     * requests received from the outer world, validating and routing
-     * them.
+     * This HTTP-related method is responsible for handling the requests of subscription
+     * definitions.
      *
      * @param routingContext routing context as provided by vertx-web
      */
-    private void handleRequest(RoutingContext routingContext) {
+    private void handleGetSubscription(RoutingContext routingContext) {
         routingContext.response().setStatusCode(OK.code()).end("Ok!");
+    }
+
+    /**
+     * This HTTP-related method is responsible for handling the creation and modification of
+     * subscriptions.
+     *
+     * @param routingContext routing context as provided by vertx-web
+     */
+    private void handlePutSubscription(RoutingContext routingContext) {
+
+    }
+
+    /**
+     * This HTTP-related method is responsible for handling the sending of messages to subscribers.
+     *
+     * @param routingContext routing context as provided by vertx-web
+     */
+    private void handlePostMessage(RoutingContext routingContext) {
+
     }
 }
